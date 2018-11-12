0bf9ce51e5a4f374c04625336006cecff53fc942
gamesbykevin <kevin@gamesbykevin.com>
2018-11-12 00:31:48 -0600

    Look for config file
    Exclude config file from .jar when packaging

--- src/main/java/com/gamesbykevin/cryptobot/util/Properties.java
+++ src/main/java/com/gamesbykevin/cryptobot/util/Properties.java
@@ -1,7 +1,10 @@
 package com.gamesbykevin.cryptobot.util;
 
+import java.io.FileInputStream;
+
 public class Properties {
 
+    //object used to access our properties
     private static java.util.Properties PROPERTIES;
 
     //location of property file
@@ -11,15 +14,38 @@ public class Properties {
 
         try {
 
+            //if null, instantiate and load
             if (PROPERTIES == null) {
+
+                //loading...
+                System.out.println("Loading properties: " + PROPERTY_FILE);
+
+                //create our object
                 PROPERTIES = new java.util.Properties();
-                PROPERTIES.load(Properties.class.getClassLoader().getResourceAsStream(PROPERTY_FILE));
+
+                try {
+
+                    //load file from same directory
+                    PROPERTIES.load(new FileInputStream(PROPERTY_FILE));
+
+                } catch (Exception e) {
+
+                    //if we couldn't find the property file we must be testing in our IDE
+                    PROPERTIES.load(Properties.class.getClassLoader().getResourceAsStream(PROPERTY_FILE));
+
+                }
             }
 
+            //return our property
             return PROPERTIES.getProperty(key);
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+
+            //display error
+            ex.printStackTrace();
+
+            //exit with error
+            System.exit(1);
         }
 
         return null;
